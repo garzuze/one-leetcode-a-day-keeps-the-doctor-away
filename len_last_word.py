3be2f741100bc9bb4b84dc2f71ccba1c59f2bee9
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-11-04 08:19:17 -0300

    feat: add length of last word

---
+++ len_last_word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s_list = s.strip().split()
+        return len(s_list[-1])
+
+s = Solution()
+s.lengthOfLastWord("   fly me   to   the moon  ")

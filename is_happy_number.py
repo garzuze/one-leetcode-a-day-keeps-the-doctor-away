e073d5fe8d502ec077453738f1dc48794803d1c4
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-11-15 09:57:22 -0300

    feat: add is happy number

---
+++ is_happy_number.py
@@ -0,0 +1,16 @@
+class Solution:
+    count = 0
+    def isHappy(self, n: int) -> bool:
+        sum = 0
+        if self.count > 10:
+            return False
+        for number in str(n):
+            sum += pow(int(number), 2)
+        if sum == 1:
+            return True
+        else:
+            self.count += 1
+            return self.isHappy(sum)
+
+# Beats 100.00% 👏👏👏
+# solução cagada da porra kkkkkkk

be4bf716b853cfb10ac99c670e41ea191591256d
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-12-06 18:25:46 -0300

    refactor: improve product of array except self

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -2,27 +2,23 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = []
-        total_prod = 1
-        count_zero = 0
+        # intuiton = first pass: product of all number left to right
+        # second pass = product of all numbers right to left
 
-        for num in nums:
-            if num == 0:
-                count_zero += 1
-                continue
-            total_prod = total_prod * num
+        len_nums = len(nums)
+        left = 1
+        result = [1] * len_nums
+        for i in range(len_nums):
+            result[i] *= left
+            left *= nums[i]
 
-        for num in nums:
-            if count_zero == 1:
-                if num != 0:
-                    output.append(0)
-                else:
-                    output.append(total_prod)
-            elif count_zero > 1:
-                output.append(0)
-            else:
-                if num != 0:
-                    output.append(total_prod // num)
-                else:
-                    output.append(total_prod)
-        return output
+        right = 1
+        for i in range(len_nums - 1, -1, -1):
+            result[i] *= right
+            right *= nums[i]
+
+        return result
+    
+
+s = Solution()
+print(s.productExceptSelf([2,3,4,0,1]))

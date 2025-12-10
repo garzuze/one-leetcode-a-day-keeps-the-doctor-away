9eb5047872f895dc67b7f69219508232536441b4
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-12-09 23:12:55 -0300

    refactor: improve longest consecutive sequence

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -5,22 +5,15 @@ class Solution:
         if len(nums) == 0:
             return 0
 
-        seen = set()
-        possible = []
-        sequences = []
-        for num in nums:
-            seen.add(num)
-            if num - 1 not in seen:
-                possible.append(num)
-
-
-        for poss in possible:
-            count = 1
-            expected = poss + 1
-            while expected in seen:
-                count += 1
-                expected += 1
-            
-            sequences.append(count)
-
-        return max(sequences)
+        seen = set(nums)
+        longuest = 0
+        for num in seen:
+            if (num - 1) not in seen:
+                curr = 1
+                desired = num + 1
+                while desired in seen:
+                    curr += 1
+                    desired += 1
+                longuest = max(longuest, curr)
+        
+        return longuest

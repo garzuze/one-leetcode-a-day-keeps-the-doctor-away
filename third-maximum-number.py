36b5725c9592c324e1c26a8eed3a0fe2add7eef2
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-12-12 16:29:33 -0300

    refactor: improve third maximum number

--- third-maximum-number.py
+++ third-maximum-number.py
@@ -3,14 +3,25 @@ from typing import List
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        nums_set = set(nums)
-        if len(nums_set) < 3:
-            return max(nums_set)
+        first = second = third = None
 
-        nums_set.remove(max(nums_set))
-        nums_set.remove(max(nums_set))
-        return max(nums_set)
+        for num in nums:
+            if num == first or num == second or num == third:
+                continue
+            if first is None or num > first:
+                third = second
+                second = first
+                first = num
+            elif second is None or num > second:
+                third = second
+                second = num
+            elif third is None or num > third:
+                third = num
 
+        if third is not None:
+            return third
+        
+        return first
 
 s = Solution()
 print(s.thirdMax([3, 2, 1]))

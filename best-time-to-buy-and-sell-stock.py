61f69a0601c6f16e97e4135fb7d3d73f916c8734
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-12-14 11:48:32 -0300

    refactor: improve best time to buy sell and stock

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -5,14 +5,14 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         left, right = 0, 1
-        max_profit = 0
-
+        profit = 0
         while right < len(prices):
             if prices[right] < prices[left]:
                 left = right
-            else:
+
+            if prices[right] - prices[left] > profit:
                 profit = prices[right] - prices[left]
-                max_profit = max(max_profit, profit)
+            
             right += 1
 
-        return max_profit
+        return profit

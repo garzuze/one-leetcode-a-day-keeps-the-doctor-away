ed9bfa9b2a238c4f80b95c5b1e8691ab31db5fd6
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2026-01-05 09:10:55 -0300

    feat: add divide-array-into-equal-pairs

---
+++ divide-array-into-equal-pairs.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+# O(n log n) but runs faster than the other solution due to python internals
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for pair in zip(nums[::2], nums[1::2]):
+            if len(set(pair)) != 1:
+                return False
+
+        return True
+
+
+# O(n)
+class Solution2:
+    def divideArray(self, nums: List[int]) -> bool:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        for value in count.values():
+            if value % 2 != 0:
+                return False
+
+        return True

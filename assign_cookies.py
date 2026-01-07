a5047fd5676d6da3d925a1935ec15e025e2de394
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2026-01-07 09:22:05 -0300

    refactor: improve assign cookies

--- assign_cookies.py
+++ assign_cookies.py
@@ -8,14 +8,10 @@ class Solution:
 
         g_pointer = 0
         s_pointer = 0
-        count = 0
 
         while g_pointer < len(g) and s_pointer < len(s):
             if s[s_pointer] >= g[g_pointer]:
-                count += 1
-                s_pointer += 1
                 g_pointer += 1
-            else:
-                s_pointer += 1
+            s_pointer += 1
 
-        return count
+        return g_pointer

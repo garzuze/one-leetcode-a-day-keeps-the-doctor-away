44eda81188b5bb1b0b7686785695f76ddaf4b44e
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2026-01-12 08:27:54 -0300

    perf: improve performance in valid sudoku

--- valid-sudoku.py
+++ valid-sudoku.py
@@ -5,32 +5,29 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         squares = {}
 
-        for row in board:
-            row_set = set()
-            for number in row:
-                if number != ".":
-                    if number in row_set:
-                        return False
-                    row_set.add(number)
-
         for i in range(len(board)):
             column_set = set()
-            for j in range(len(board)):
-                number = board[j][i]
+            row_set = set()
 
-                if number != ".":
-                    if number in column_set:
+            for j in range(len(board)):
+                col_number = board[j][i]
+                row_number = board[i][j]
+
+                if row_number != ".":
+                    if row_number in row_set:
                         return False
-                    column_set.add(number)
+                    row_set.add(row_number)
+
+                if col_number != ".":
 
                     square_index = (j // 3) * 3 + (i // 3)
                     squares_set = squares.get(square_index, set())
 
-                    if number in squares_set:
+                    if col_number in column_set or col_number in squares_set:
                         return False
 
-                    squares_set.add(number)
-
+                    column_set.add(col_number)
+                    squares_set.add(col_number)
                     squares[square_index] = squares_set
 
         return True

7dcc9136a84abcaeaee44702387c3757a1844379
garzuze <lucasgarzuze@gmail.com>
2026-01-29 22:49:42 -0300

    perf: improve find-words-that-can-be-formed-by-characters

--- find-words-that-can-be-formed-by-characters.py
+++ find-words-that-can-be-formed-by-characters.py
@@ -4,19 +4,19 @@ from typing import List
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         result = 0
-        chars_map = {}
+        chars_map = [0] * 26
+        BASE = ord('a')
 
-        for char in chars:
-            chars_map[char] = chars_map.get(char, 0) + 1
+        for ch in chars:
+            chars_map[ord(ch) - BASE] += 1
 
         for word in words:
-            word_map = {}
+            word_map = [0] * 26
             is_good = True
             for ch in word:
-                word_map[ch] = word_map.get(ch, 0) + 1
-
-            for ch in word_map:
-                if ch not in chars_map or word_map[ch] > chars_map[ch]:
+                i = ord(ch) - BASE
+                word_map[i] += 1
+                if word_map[i] and word_map[i] > chars_map[i]:
                     is_good = False
                     break
 

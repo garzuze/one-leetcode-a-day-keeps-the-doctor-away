a7f340bdec1fdd8dcf19414e22a1ee8ec7ee23eb
Lucas Garzuze Cordeiro <118022446+garzuze@users.noreply.github.com>
2026-02-17 20:58:57 +0000

    feat: add reorder-list

---
+++ reorder-list.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        current = slow.next
+        slow.next = None
+        prev = None
+
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+
+        first, second = head, prev
+
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2

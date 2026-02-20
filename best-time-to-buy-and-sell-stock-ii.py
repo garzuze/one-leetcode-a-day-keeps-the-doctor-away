c3d63f730ed905d48695eabb5762123b18ea9630
garzuze <lucasgarzuze@gmail.com>
2026-02-19 22:02:10 -0300

    perf: improve best-time-to-buy-and-sell-stock-ii performance

--- best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii.py
@@ -4,15 +4,15 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         left, right = 0, 1
-        profits = []
+        profit = 0
 
         while right < len(prices):
             if prices[left] > prices[right]:
                 left = right
             elif prices[right] > prices[left]:
-                profits.append(prices[right] - prices[left])
+                profit += prices[right] - prices[left]
                 left = right
 
             right += 1
 
-        return sum(profits)
+        return profit

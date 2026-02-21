d2014a8f9903daccfed56a6cf8d79e289f53030b
garzuze <lucasgarzuze@gmail.com>
2026-02-21 12:43:42 -0300

    perf: improve group-the-people-given-the-group-size-they-belong-to.py

--- group-the-people-given-the-group-size-they-belong-to.py
+++ group-the-people-given-the-group-size-they-belong-to.py
@@ -4,17 +4,15 @@ from typing import List
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         groups = {}
+        result = []
         for i, size in enumerate(groupSizes):
             if size not in groups:
                 groups[size] = []
 
             groups[size].append(i)
 
-        result = []
-        for group, people in groups.items():
-            if len(people) > group:
-                result += [people[i:i + group] for i in range(0, len(people), group)]
-            else:
-                result.append(people)
+            if len(groups[size]) == size:
+                result.append(groups[size])
+                groups[size] = []
 
         return result

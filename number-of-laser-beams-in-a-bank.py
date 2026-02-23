9d7515717de0d7e495cf7b9b636f310001695a92
garzuze <lucasgarzuze@gmail.com>
2026-02-22 22:21:41 -0300

    perf: improve number-of-laser-beams-in-a-bank

--- number-of-laser-beams-in-a-bank.py
+++ number-of-laser-beams-in-a-bank.py
@@ -3,21 +3,14 @@ from typing import List
 
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        rows = []
+        prev = None
+        result = 0
 
         for line in bank:
             count = line.count("1")
             if count != 0:
-                rows.append(count)
-
-        if len(rows) == 0:
-            return 0
-
-        prev = rows[0]
-        result = 0
-
-        for i in range(1, len(rows)):
-            result += prev * rows[i]
-            prev = rows[i]
+                if prev is not None:
+                    result += count * prev
+                prev = count
 
         return result

3a9a9fde7b15b4e49d29e1fd11403d1331c61976
garzuze <lucasgarzuze@gmail.com>
2026-02-24 15:13:50 -0300

    refactor: make subsets more simple

--- subsets.py
+++ subsets.py
@@ -4,18 +4,14 @@ from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
-        subset = []
-        self.backtrack(0, nums, result, subset)
 
+        def backtrack(start: int, sub_set: List[int]):
+            result.append(list(sub_set))
+
+            for i in range(start, len(nums)):
+                sub_set.append(nums[i])
+                backtrack(i + 1, sub_set)
+                sub_set.pop()
+
+        backtrack(0, [])
         return result
-
-    def backtrack(self, i, arr: List[int], result: List[List[int]], subset: List[int]):
-        if i == len(arr):
-            result.append(list(subset))
-            return
-
-        subset.append(arr[i])
-        self.backtrack(i + 1, arr, result, subset)
-
-        subset.pop()
-        self.backtrack(i + 1, arr, result, subset)

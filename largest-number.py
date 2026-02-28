5b030338ffc7d8ceb8a33d043884423e20691927
Lucas Garzuze Cordeiro <118022446+garzuze@users.noreply.github.com>
2026-02-28 13:45:49 -0300

    feat: add largest-number

---
+++ largest-number.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums_str = [str(num) for num in nums]
+
+        nums_str.sort(key=lambda x: x * 10, reverse=True)
+
+        if nums_str[0] == "0":
+            return "0"
+
+        return "". join(nums_str)

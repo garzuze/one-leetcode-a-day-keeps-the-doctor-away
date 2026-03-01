68c7831b8be8225e240955360bdb2f040c905c6f
garzuze <lucasgarzuze@gmail.com>
2026-03-01 16:14:38 -0300

    refactor: make smallest-value-of-the-rearranged-number more simple

--- smallest-value-of-the-rearranged-number.py
+++ smallest-value-of-the-rearranged-number.py
@@ -4,28 +4,20 @@ class Solution:
             return self._process_negative(num)
         return self._process_positive(num)
 
-    def _process_negative(self, num: int) -> int:
-        digits = [digit for digit in str(num) if digit != "-"]
-
-        digits.sort(key=lambda x: x * 10, reverse=True)
-
-        return - int("".join(digits))
-
     def _process_positive(self, num: int) -> int:
-        digits = []
-        zeros = 0
+        digits = sorted(str(num))
 
-        for digit in str(num):
-            if digit == "0":
-                zeros += 1
-            else:
-                digits.append(digit)
-
-        digits.sort(key=lambda x: x * 10)
-
-        if not digits or digits[0] == "0":
+        if digits[0] == "0" and len(digits) == 1:
             return 0
 
-        digits = [digits[0]] + ["0"] * zeros + digits[1:]
+        i = 0
+        while digits[0] == "0":
+            digits[0], digits[i] = digits[i], "0"
+            i += 1
 
         return int("".join(digits))
+
+    def _process_negative(self, num: int) -> int:
+        digits = sorted(str(-num), reverse=True)
+
+        return - int("".join(digits))

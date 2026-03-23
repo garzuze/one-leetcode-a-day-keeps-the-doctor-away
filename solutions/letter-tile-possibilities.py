836bbfae992e2112b5ebbbf60dbe80b886623052
garzuze <lucasgarzuze@gmail.com>
2026-03-23 11:37:16 -0300

    chore: remove print from letter-tile-possibilities

--- solutions/letter-tile-possibilities.py
+++ solutions/letter-tile-possibilities.py
@@ -8,7 +8,6 @@ class Solution:
         return len(self.result) - 1
     
     def backtrack(self, prefix: str, curr: str):
-        print(self.result)
         if prefix in self.result:
             return
 

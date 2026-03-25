c1f9d10e118cdcdb850e074196e7e5de4b42a30f
garzuze <lucasgarzuze@gmail.com>
2026-03-25 08:40:16 -0300

    refactor: slightly improve subarray-sum-equals-k

--- solutions/subarray-sum-equals-k.py
+++ solutions/subarray-sum-equals-k.py
@@ -1,19 +1,20 @@
 from typing import List
 
 
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefix_sum = {}
         count = 0
         curr_sum = 0
 
+        prefix_sum[0] = 1
+
         for i in range(len(nums)):
             curr_sum += nums[i]
             
             if curr_sum - k in prefix_sum:
                 count += 1 * prefix_sum.get(curr_sum - k)
-            if curr_sum == k:
-                count += 1
             
             prefix_sum[curr_sum] = prefix_sum.get(curr_sum, 0) + 1
             

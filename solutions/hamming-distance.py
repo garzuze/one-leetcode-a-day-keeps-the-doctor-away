5e99c0614d17ad2e2a8564e4b37b1a321472491f
garzuze <lucasgarzuze@gmail.com>
2026-04-25 13:30:00 -0300

    refactor: improve hamming-distance

--- solutions/hamming-distance.py
+++ solutions/hamming-distance.py
@@ -1,19 +1,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        bin_rep_x = bin(x).replace("b", "")
-        bin_rep_y = bin(y).replace("b", "")
-
-        diff = abs(len(bin_rep_x) - len(bin_rep_y))
-
-        if len(bin_rep_x) < len(bin_rep_y):
-            bin_rep_x = ("0" * diff) + bin_rep_x
-        else:
-            bin_rep_y = ("0" * diff) + bin_rep_y
-        
+        xor = x ^ y
         count = 0
 
-        for i in range(len(bin_rep_y)):
-            if bin_rep_y[i] != bin_rep_x[i]:
-                count += 1
-            
+        while xor:
+            count += 1
+            xor &= xor - 1
+
         return count

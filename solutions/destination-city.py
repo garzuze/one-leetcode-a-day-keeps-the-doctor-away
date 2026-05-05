291640dce002221ea4fe1d9b96864c53c5419adc
garzuze <lucasgarzuze@gmail.com>
2026-05-05 14:55:45 -0300

    feat: add destination-city

---
+++ solutions/destination-city.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        seen = set()
+        for path in paths:
+            seen.add(path[0])
+        
+        for path in paths:
+            if path[1] not in seen:
+                return path[1]
+
+        return ""

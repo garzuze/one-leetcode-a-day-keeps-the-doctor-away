88e1c835792df08a07e95c94e1d3f2a0c06e4f04
garzuze <lucasgarzuze@gmail.com>
2026-05-31 17:14:35 -0300

    feat: add destroying-asteroids

---
+++ solutions/destroying-asteroids.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+
+        for a in asteroids:
+            if mass >= a:
+                mass += a
+            else:
+                return False
+        
+        return True

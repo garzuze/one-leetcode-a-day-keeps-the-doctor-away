e7258f0f8978df6bfde9d3597fa5aa8c02d97f53
garzuze <lucasgarzuze@gmail.com>
2026-07-01 15:56:31 -0300

    feat: add find-triangular-sum-of-an-array

---
+++ solutions/find-triangular-sum-of-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        for (int j = nums.length - 1; j >= 0; j--) {
+            for (int i = 0; i < j; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}

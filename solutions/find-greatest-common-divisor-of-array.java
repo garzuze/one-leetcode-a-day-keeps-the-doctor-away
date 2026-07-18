ff11ff64005b16d5b72bdbb503213769d7a56975
garzuze <lucasgarzuze@gmail.com>
2026-07-18 16:51:18 -0300

    feat: add find-greatest-common-divisor-of-array

---
+++ solutions/find-greatest-common-divisor-of-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        return gcd(min, max);
+    }
+
+    private static int gcd(int a, int b) {
+        int temp = 0;
+
+        while (b != 0) {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}

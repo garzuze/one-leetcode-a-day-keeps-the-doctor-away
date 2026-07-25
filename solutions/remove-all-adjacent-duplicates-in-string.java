4a28d86216855afd4667279fe0f96fe076460873
garzuze <lucasgarzuze@gmail.com>
2026-07-25 16:27:46 -0300

    feat: add remove-all-adjacent-duplicates-in-string

---
+++ solutions/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,25 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek().equals(ch)) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+    }
+}

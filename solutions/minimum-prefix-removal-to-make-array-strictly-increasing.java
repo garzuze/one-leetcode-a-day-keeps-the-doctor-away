efe506eb24c4e20153b8d69e7ced9622af50a33c
Lucas Garzuze Cordeiro <118022446+garzuze@users.noreply.github.com>
2026-08-11 22:02:56 -0300

    feat: add minimum-prefix-removal-to-make-array-strictly-increasing

---
+++ solutions/minimum-prefix-removal-to-make-array-strictly-increasing.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumPrefixLength(int[] nums) {
+        for (int i = nums.length - 1; i >= 1; i--) {
+            if (nums[i - 1] >= nums[i]) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}

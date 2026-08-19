55ede6c7c121ddb6e541f2c0df751caaf88eb478
garzuze <lucasgarzuze@gmail.com>
2026-08-19 15:26:50 -0300

    refactor: simplify maximum-median-sum-of-subsequences-of-size-3

--- solutions/maximum-median-sum-of-subsequences-of-size-3.java
+++ solutions/maximum-median-sum-of-subsequences-of-size-3.java
@@ -1,28 +1,15 @@
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.ArrayDeque;
-
 
 class Solution {
     public long maximumMedianSum(int[] nums) {
         long result = 0;
         Arrays.sort(nums);
+        int init = nums.length / 3;
 
-        if (nums.length == 3) {
-            return nums[1];
+        for (int i = init; i < nums.length; i += 2) {
+            result += nums[i];
         }
-
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            deque.addLast(nums[i]);
-        }
-
-        while (!deque.isEmpty()) {
-            deque.removeFirst();
-            deque.pollLast();
-            result += deque.pollLast();
-        }
+        
 
         return result;
     }

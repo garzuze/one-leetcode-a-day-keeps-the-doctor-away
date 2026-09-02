90fa06e1ea0b0b68744c9ae9e7a8e900e1b7b121
garzuze <lucasgarzuze@gmail.com>
2026-09-01 23:49:02 -0300

    feat: add shuffle-the-array

---
+++ solutions/shuffle-the-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            result[j] = nums[i];
+            result[j + 1] = nums[i + n];
+            j+=2;
+        }
+
+        return result;
+    }
+}

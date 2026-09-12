802919eab2128c1d5b635c2658281ca02fd83848
garzuze <lucasgarzuze@gmail.com>
2026-09-12 16:12:46 -0300

    perf: improve reducing-dishes

--- solutions/reducing-dishes.java
+++ solutions/reducing-dishes.java
@@ -4,19 +4,20 @@ class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
         int n = satisfaction.length;
-        int max = Integer.MIN_VALUE;
+        int acc = 0;
+        int result = 0;
         
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            int k = 0;
-            for (int j = i; j < n; j++) {
-                sum += satisfaction[j] * (k + 1);
-                k++;
+        for (int i = n - 1; i >= 0; i--) {
+            acc += satisfaction[i];
+            
+            if (acc <= 0) {
+                break;
             }
 
-            max = Math.max(max, sum);
+            result += acc;
         }
 
-        return max > 0 ? max : 0;
+
+        return result;
     }
 }

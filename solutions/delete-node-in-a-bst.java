8e811dd964e57b29e395a0c5cb64a44ebff62b4c
garzuze <lucasgarzuze@gmail.com>
2026-09-23 08:50:21 -0300

    feat: add delete-node-in-a-bst

---
+++ solutions/delete-node-in-a-bst.java
@@ -0,0 +1,51 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+            return root;
+        }
+
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+            return root;
+        }
+
+        if (root.left == null && root.right == null) {
+            return null;
+        }
+
+        if (root.left == null) {
+            return root.right;
+        }
+
+        if (root.right == null) {
+            return root.left;
+        }
+
+        TreeNode prev = root.left;
+
+        while (prev.right != null) {
+            prev = prev.right;
+        }
+
+        root.val = prev.val;
+        root.left = deleteNode(root.left, prev.val);
+
+        return root;
+    }
+}
